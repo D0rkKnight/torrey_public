15e1440babbdaba19042deaa17b64829482f74fd
Hanzen Shou <heshou@ucsd.edu>
2023-05-26 04:27:00 -0700

    Blinn phong done

--- src/custom/blinn_phong.cpp
+++ src/custom/blinn_phong.cpp
@@ -21,25 +21,29 @@ bool BlinnPhongMaterial::scatter(const Ray &ray, const RayHit &hit, Vector3 &alb
     Real u1 = next_pcg32_real<Real>(rng);
     Real u2 = next_pcg32_real<Real>(rng);
 
+    // Randomly sample half vector for basis ns
     Real cosTheta = pow(u1, 1 / (exp + 1));
-    Real phi = 2 * MY_PI * u2;
 
-    Vector3 reflected = reflect(normalize(ray.dir), hit.normal);
-    onb uvw = onb();
-    uvw.build_from_w(reflected);
+    // Real cosTheta = pow(u1, 1 / (exp + 1));
+    // Real phi = 2 * MY_PI * u2;
 
-    // Get ray in aroundR space from cosTheta and phi
-    Real z = cosTheta;
-    Real sinTheta = sqrt(1 - cosTheta * cosTheta);
-    Real x = cos(phi) * sinTheta;
-    Real y = sin(phi) * sinTheta;
+    // Vector3 reflected = reflect(normalize(ray.dir), hit.normal);
+    // onb uvw = onb();
+    // uvw.build_from_w(reflected);
 
-    Vector3 direction = uvw.local(Vector3{x, y, z});
-    scattered = Ray(ray * hit.t, direction);
-    alb = getTexColor(hit.u, hit.v);
+    // // Get ray in aroundR space from cosTheta and phi
+    // Real z = cosTheta;
+    // Real sinTheta = sqrt(1 - cosTheta * cosTheta);
+    // Real x = cos(phi) * sinTheta;
+    // Real y = sin(phi) * sinTheta;
 
-    pdf = (exp + 1) * pow(dot(direction, reflected), exp) / (2 * MY_PI);
-    return true;
+    // Vector3 direction = uvw.local(Vector3{x, y, z});
+    // scattered = Ray(ray * hit.t, direction);
+    // alb = getTexColor(hit.u, hit.v);
+
+    // pdf = (exp + 1) * pow(dot(direction, reflected), exp) / (2 * MY_PI);
+    // return true;
+
 }
 
 double BlinnPhongMaterial::scattering_pdf(
@@ -48,7 +52,7 @@ double BlinnPhongMaterial::scattering_pdf(
     auto half = normalize(-r_in.dir + scattered.dir);
     auto cosine = dot(half, rec.normal);
 
-    return cosine < 0 ? 0 : pow(cosine, exp) * (exp + 1) / (2 * MY_PI * 4 * dot(r_in.dir, half)) ;
+    return cosine < 0 ? 0 : pow(cosine, exp) * (exp + 1) / (2 * MY_PI * 4 * dot(scattered.dir, half)) ;
 
     // auto reflectRay = Ray(r_in * rec.t, normalize(reflect(normalize(r_in.dir), rec.normal)));
     // auto cosine = dot(reflectRay.dir, scattered.dir);
